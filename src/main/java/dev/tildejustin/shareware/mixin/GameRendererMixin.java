e33a149a03a3ca6f2abeda1f769fc109c8116f6d
Claire <me@skye.lol>
2024-09-12 11:16:52 -0500

    disable "unregistered version" text

---
+++ src/main/java/dev/tildejustin/shareware/mixin/GameRendererMixin.java
@@ -0,0 +1,15 @@
+package dev.tildejustin.shareware.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;method_20281()V"))
+    private void render(GameRenderer renderer, float f, long l, boolean bl) {
+
+    }
+}
