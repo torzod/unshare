cde7b1eedfa68a25706d60b3bb300d3922637d42
ouroya <mctorzod@gmail.com>
2024-09-12 01:14:15 -0500

    start resource pack work

---
+++ src/main/java/dev/tildejustin/shareware/Unshade.java
@@ -0,0 +1,11 @@
+package dev.tildejustin.shareware;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Unshade implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
